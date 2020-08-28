f07a91b2260c47a4e964a0f65e8c97a023f12c45
Jorge Castello <60675751+jorgio-castello@users.noreply.github.com>
2020-08-28 14:38:38 -0700

    Create server dir, fetchUniverities helper

---
+++ server/config.js
@@ -0,0 +1,3 @@
+const UNIVERSITY_FILENAME = process.env.UNIVERSITY_FILENAME || 'trieData.txt';
+const UNIVERSITY_SEARCH_URL = 'http://universities.hipolabs.com/search?name=';
+module.exports = { UNIVERSITY_FILENAME, UNIVERSITY_SEARCH_URL };
---
+++ server/helpers/fetchUniversities.js
@@ -0,0 +1,38 @@
+const alphabet = require('../model/alphabet');
+const path = require('path');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const os = require('os');
+const { UNIVERSITY_FILENAME, UNIVERSITY_SEARCH_URL } = require('../config');
+
+const UNIVERSITY_OUTPUT_FILEPATH = path.join(__dirname, `../data/${UNIVERSITY_FILENAME}`);
+
+async function fetchUniversities(letter) {
+  return fetch(`${UNIVERSITY_SEARCH_URL}${letter}`).then(res => res.json());
+}
+
+const init = (universityObj, idx, callback) => {
+  if (idx === alphabet.length) {
+    callback(universityObj);
+    return;
+  }
+  const letter = alphabet[idx];
+  console.log(`Fetching results for ${letter}`);
+  fetchUniversities(letter)
+    .then(data => {
+      data.map(university => universityObj[university.name] = true);
+      init(universityObj, idx + 1, callback);
+    });
+}
+
+// UniversityObj, alphabet start idx, callback to write data to file
+init({}, 0, (universityData) => {
+  let universities = Object.keys(universityData); // raw data as an arr
+  universities = universities.join(os.EOL); // package the universities as a string, delimited by operating system end of line
+  fs.appendFile(UNIVERSITY_OUTPUT_FILEPATH, universities, err => {
+    if (err) {
+      throw new Error(err);
+    }
+    console.log('University data has been successfully written to disk');
+  });
+});
